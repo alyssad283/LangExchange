0d81ea1b8e94e9bebb1ff0c023b22f8a5929fc56
Alyssa Dominico <59928555+alyssad283@users.noreply.github.com>
2023-06-26 04:25:30 -0500

    Initialize hide-in-chat-translation

---
+++ app/src/main/java/edu/temple/langexchange/ChatRoomChoice.java
@@ -0,0 +1,192 @@
+package edu.temple.langexchange;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.EventListener;
+
+public class ChatRoomChoice extends AppCompatActivity {
+
+    //    Button btnSpa, btnGer, btnEng, btnFre, goToFlashcards;
+    Spinner spin;
+    Button submitBtn;
+    ListView listView;
+    ArrayList<String> availableRooms = new ArrayList<>();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chatroom_choice);
+
+//        btnSpa = findViewById(R.id.SpanishRoomBtn);
+//        btnEng = findViewById(R.id.EnglishRoomBtn);
+//        btnGer = findViewById(R.id.GermanRoomBtn);
+//        btnFre = findViewById(R.id.FrenchRoomBtn);
+//        goToFlashcards = findViewById(R.id.go_to_flashcards_btn);
+
+        spin = findViewById(R.id.spinner);
+        submitBtn = findViewById(R.id.selectLangBtn);
+        listView = findViewById(R.id.availableRoom);
+
+        Intent prevIntent = getIntent();
+        String userName = prevIntent.getStringExtra("username");
+//        int userId = prevIntent.getIntExtra("userId", 0);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, Translator.getLanguages());
+        spin.setAdapter(adapter);
+
+
+
+        availableRooms.add("");
+        DatabaseReference roomRef = FirebaseDatabase.getInstance().getReference().child("ChatRoom");
+        roomRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                availableRooms.clear();
+                if(snapshot.hasChildren())
+                {
+                    for (DataSnapshot childSnap : snapshot.getChildren())
+                    {
+                        availableRooms.add(childSnap.child("langChosen").getValue().toString());
+                    }
+                }
+                if(!availableRooms.isEmpty())
+                {
+                    ArrayAdapter adapter = new ArrayAdapter<String>(ChatRoomChoice.this, android.R.layout.simple_list_item_1, availableRooms);
+                    listView.setAdapter(adapter);
+                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            Intent intent = new Intent(ChatRoomChoice.this, ChatSystem.class);
+                            intent.putExtra("langSelected", availableRooms.get(position));
+                            System.out.println("Available Room passed: " + availableRooms.get(position));
+                            startActivity(intent);
+                        }
+                    });
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String selectedLang = spin.getSelectedItem().toString();
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("ChatRoom");
+                ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        ArrayList<String> usedLang = new ArrayList<>();
+                        usedLang.add("");
+                        for(DataSnapshot childSnap : snapshot.getChildren())
+                        {
+                            if(snapshot.getChildren() != null)
+                            {
+                                usedLang.add(childSnap.child("langChosen").getValue().toString());
+                                System.out.println("used lang:" + usedLang);
+                            }
+                        }
+
+                        if (!usedLang.contains(selectedLang)) {
+                            Intent intent = new Intent(ChatRoomChoice.this, ChatSystem.class);
+                            intent.putExtra("langSelected", selectedLang);
+                            finish();
+                            startActivity(intent);
+                        } else if(usedLang.contains(selectedLang)){
+                            Toast.makeText(ChatRoomChoice.this, "Room already exist", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+//        btnSpa.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(ChatRoomChoice.this, ChatSystem.class);
+//                intent.putExtra("channelID", "K37YpRtGTMBC9JAZ");
+//                intent.putExtra("langSelected", "SPANISH");
+//                intent.putExtra("username", userName);
+//
+//                startActivity(intent);
+//            }
+//        });
+//
+//        btnGer.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(ChatRoomChoice.this, ChatSystem.class);
+//                intent.putExtra("channelID", "iTzl5dVNhZweOFTo");
+//                intent.putExtra("langSelected", "GERMAN");
+//                intent.putExtra("username", userName);
+//
+//                startActivity(intent);
+//            }
+//        });
+//
+//        btnEng.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(ChatRoomChoice.this, ChatSystem.class);
+//                intent.putExtra("channelID", "9Re6IIi9ZhoqxGbc");
+//                intent.putExtra("langSelected", "ENGLISH");
+//                intent.putExtra("username", userName);
+//
+//                startActivity(intent);
+//            }
+//        });
+//
+//        btnFre.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(ChatRoomChoice.this, ChatSystem.class);
+//                intent.putExtra("channelID", "Pbf9jcw2NrgUxB2B");
+//                intent.putExtra("langSelected", "FRENCH");
+//                intent.putExtra("username", userName);
+//
+//                startActivity(intent);
+//            }
+//        });
+//
+//        goToFlashcards.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(ChatRoomChoice.this, FlashcardActivity.class);
+//                intent.putExtra("username", userName);
+//                intent.putExtra("userId", userId);
+//                startActivity(intent);
+//            }
+//        });
+    }
+}
---
+++ app/src/main/java/edu/temple/langexchange/Translator.java
@@ -0,0 +1,200 @@
+package edu.temple.langexchange;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.StrictMode;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Translator {
+    private static String originalText;
+    private static String translatedText;
+    private static boolean connected;
+    private static Translate translate;
+    final static Map<String, String> languageCodes = new HashMap<String, String>(){{
+        put("Arabic", "ar");
+        put("Armenian", "hy");
+        put("Azerbaijani", "az");
+        put("Bengali", "bn");
+        put("Chinese", "zh");
+        put("Chinese(Simplified)", "zh");
+        put("Chinese(Traditional)", "zh-TW");
+        put("Danish", "da");
+        put("English", "en");
+        put("Finnish", "fi");
+        put("Filipino", "fil");
+        put("French", "fr");
+        put("German", "de");
+        put("Gujarati", "gu");
+        put("Hebrew", "he");
+        put("Hindi", "hi");
+        put("Hungarian", "hu");
+        put("Italian", "it");
+        put("Japanese", "ja");
+        put("Korean", "ko");
+        put("Malay", "ms");
+        put("Pashto", "ps");
+        put("Persian", "fa");
+        put("Portuguese", "pt");
+        put("Punjabi", "pa");
+        put("Russian", "ru");
+        put("Spanish", "es");
+        put("Swahili", "sw");
+        put("Swedish", "sv");
+        put("Tagalog", "tl");
+        put("Tamil", "ta");
+        put("Thai", "th");
+        put("Turkish", "tr");
+        put("Urdu", "ur");
+        put("Vietnamese", "vi");
+        put("Xhosa", "xh");
+        put("Yoruba", "yo");
+        put("Zulu", "zu");
+
+
+    }};
+
+    final static Map<String, String> audioLanguageCodes = new HashMap<String, String>(){{
+        put("Arabic", "ar-XA");
+        put("Bengali", "bn-IN");
+        put("Chinese", "cmn-CN");
+        put("Chinese(Simplified)", "zh-CN");
+        put("Chinese(Traditional)", "zh-TW");
+        put("Danish", "da-DK");
+        put("English", "en-US");
+        put("Filipino", "fil-PH");
+        put("Finnish", "fi-FI");
+        put("French", "fr-FR");
+        put("German", "de-DE");
+        put("Gujarati", "gu-IN");
+        put("Hindi", "hi-IN");
+        put("Hungarian", "hu-HU");
+        put("Italian", "it-IT");
+        put("Japanese", "ja-JP");
+        put("Korean", "ko-KR");
+        put("Malay", "ml-IN");
+        put("Portuguese", "pt-BR");
+        put("Punjabi", "pa");
+        put("Russian", "ru-RU");
+        put("Spanish", "es-US");
+        put("Swedish", "sv-SE");
+        put("Tamil", "ta-IN");
+        put("Thai", "th-TH");
+        put("Turkish", "tr-TR");
+        put("Vietnamese", "vi-VN");
+    }};
+
+    final static Map<String, String> speechLanguageCodes = new HashMap<String, String>(){{
+        put("Arabic", "ar");
+        put("Bengali", "bn");
+        put("Chinese", "zh");
+        put("Chinese(Simplified)", "zh_cn");
+        put("Chinese(Traditional)", "zh_tw");
+        put("Danish", "da");
+        put("English", "en");
+        put("Filipino", "fil");
+        put("Finnish", "fi");
+        put("French", "fr");
+        put("German", "de");
+        put("Gujarati", "gu");
+        put("Hindi", "hi");
+        put("Hungarian", "hu");
+        put("Italian", "it");
+        put("Japanese", "ja");
+        put("Korean", "ko");
+        put("Malay", "ms");
+        put("Portuguese", "pt_BR");
+       // put("Punjabi", "pa");
+        put("Russian", "ru");
+        put("Spanish", "es_419");
+        put("Swedish", "sv");
+        put("Tamil", "ta");
+        put("Thai", "th");
+        put("Turkish", "tr");
+        put("Vietnamese", "vi");
+    }};
+
+    public static ArrayList<String> getLanguages(){
+        ArrayList<String>languages = new ArrayList<>();
+        for(String key : audioLanguageCodes.keySet()){
+            languages.add(key);
+
+        }
+        languages.remove("Chinese");
+        Collections.sort(languages);
+        return languages;
+    }
+
+    public static String getAudioCode(String language){
+        if(!audioLanguageCodes.containsKey(language)){
+            return "Audio messages not supported.";
+        }
+        return audioLanguageCodes.get(language);
+    }
+
+    public static String translate(String text, String prefLang, Context context) {
+        if(!languageCodes.containsKey(prefLang)){
+            return "Language not supported.";
+        }
+        if(!checkInternetConnection(context)){
+            return "No internet connection. Cannot be translated.";
+        }
+        //Connect to API
+        getTranslateService(context);
+        //Get input text to be translated:
+        originalText = text;
+        Translation translation = translate.translate(originalText, Translate.TranslateOption.targetLanguage(languageCodes.get(prefLang)), Translate.TranslateOption.model("base"));
+        translatedText = translation.getTranslatedText();
+
+        //Translated text and original text are set to TextViews:
+        return translatedText;
+
+    }
+
+    private static void getTranslateService(Context context) {
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        try (InputStream is = context.getResources().openRawResource(R.raw.translatecredentials)) {
+
+            //Get credentials:
+            final GoogleCredentials myCredentials = GoogleCredentials.fromStream(is);
+
+            //Set credentials and get translate service:
+            TranslateOptions translateOptions = TranslateOptions.newBuilder().setCredentials(myCredentials).build();
+            translate = translateOptions.getService();
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+
+        }
+    }
+
+    public static boolean checkInternetConnection(Context context) {
+
+        //Check internet connection:
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        //Means that we are connected to a network (mobile or wi-fi)
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+
+        connected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+
+        return connected;
+    }
+
+
+}
